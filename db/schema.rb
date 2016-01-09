cb9ed06d3b3545a852abc9792a41833158b3a907
Naina Raisinghani <nainar@chromium.org>
2016-01-09 19:56:30 +1100

    add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109083500) do
+ActiveRecord::Schema.define(version: 20160109084842) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160109083500) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
